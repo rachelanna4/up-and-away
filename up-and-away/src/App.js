d25633e87e772bbc013d78f86007d5dc0959aba2
Rachel <rachelanna92@outlook.com>
2021-10-07 17:35:43 +0100

    display letters when correct button is clicked

--- up-and-away/src/App.js
+++ up-and-away/src/App.js
@@ -7,16 +7,21 @@ import WordDisplay from "./components/WordDisplay";
 import { useState } from "react";
 
 function App() {
-  const wordGenerator = "mittens";
+  const wordGenerator = "MITTENS";
   const [word] = useState(wordGenerator);
   const chosenLetters = ["t"];
-  const [letters] = useState(chosenLetters);
+  const [letters, setLetters] = useState(chosenLetters);
+
+  const checkLetter = (letter) => {
+    const newLetterList = [...letters, letter];
+    setLetters(newLetterList);
+  };
 
   return (
     <div className="App">
       <Header />
       <GameImage />
-      <Keyboard />
+      <Keyboard currLetters={letters} checkLetter={checkLetter} />
       <GameStatus />
       <WordDisplay word={word} letters={letters} />
     </div>
--- up-and-away/src/components/Keyboard.jsx
+++ up-and-away/src/components/Keyboard.jsx
@@ -1,12 +1,13 @@
-const Keyboard = () => {
+const Keyboard = ({currLetters, checkLetter}) => {
 
     const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
+  
 
     return (
         <section>
         {alphabet.map(letter => {
-            return <button>{letter}</button>
+            return <button key={letter} onClick={() => checkLetter(letter) }>{letter}</button>
           })
         }
         </section>
