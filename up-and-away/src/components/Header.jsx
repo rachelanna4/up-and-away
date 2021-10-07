5d0440c998cd69fc818782783732fc6aaeba411d
Rachel <rachelanna92@outlook.com>
2021-10-07 16:46:08 +0100

    refactor header paragraph text

--- up-and-away/src/components/Header.jsx
+++ up-and-away/src/components/Header.jsx
@@ -2,9 +2,10 @@ const Header = () => {
     return (
         <section>
         <h1>Up and Away</h1>
-        <p>Save Percy the Penguin from falling from the sky by guessing the word correctly.
-        For every letter you get wrong, one of his balloons will burst. 
-        Get the word right before his last balloon pops!
+        <p>It turns out that pigs can fly... but only with the help of some balloons!
+        Help Penelope the pig stay high in the sky by guessing the word correctly. 
+        For every letter you get wrong, one of her balloons will burst. 
+        Get the word right before her last balloon pops!
         </p>
         </section>
     )
