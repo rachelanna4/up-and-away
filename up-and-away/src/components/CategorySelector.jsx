79ff319f1799af0b20e5fa3ee5c0f393672197c3
Rachel <rachelanna92@outlook.com>
2021-10-08 22:47:50 +0100

    add word-category data

--- up-and-away/src/components/CategorySelector.jsx
+++ up-and-away/src/components/CategorySelector.jsx
@@ -6,6 +6,8 @@ const CategorySelector = ({restartGame}) => {
             <option value="" selected disabled hidden>Choose a word category</option>
             <option value="animals">Animals</option>
             <option value="halloween">Halloween</option>
+            <option value="harryPotter">Harry Potter Characters</option>
+            <option value="superheroes">Superheroes and Villians</option>
         </select>
     )
 }
--- up-and-away/src/data/word-categories.js
+++ up-and-away/src/data/word-categories.js
@@ -1,15 +1,3 @@
-// const words = [
-//     // {category: "animals", words: ["giraffe", "elephant", "hippopotamus", "zebra", "gorilla", "koala bear", "kangaroo", "beaver", "badger", "mouse", "whale", "dolphin", "cheetah", "snow leopard", "wolf", "polar bear", "penguin"]}
-//     // {category: "countries", words: ["scotland", "south africa", "canada", "england", "wales", "egypt", "kenya", "thailand", "australia", "new zealand", "brazil", "spain", "portugal", "france", "india", "morocco", "iceland", "singapore", "mexico", "chile", "united states of america", "sweden", "denmark", "finland", "ireland", "cambodia"]}
-//     // {category: "books", words: ["the hobbit", "gangsta granny"]}
-//     // {category: "disney films", words: ["frozen", "pocahontas", "the little mermaid", "the jungle book", "the princess and the frog", ""]}
-//     // {category: "harry potter characters", words: ["ron weasley", "harry potter", "hermione granger", "hagrid", "dumbledore", "severus snape", "voldemort", "neville longbottom", "luna lovegood"]}
-//     // {category: "christmas", words: ["snowman", "reindeer", "santa claus", "rudolph"]}
-//     // {category: "halloween", words: ["pumpkin", ""]}
-//     // {category: "food", words: []}
-
-// ]
-
 const wordCategories = [
   {
     name: "animals",
@@ -19,6 +7,27 @@ const wordCategories = [
       { word: "whale", clue: "This animal is the largest mammal" },
       { word: "polar bear", clue: "This animal lives in the Arctic" },
       { word: "kangaroo", clue: "This animal has a pouch to carry its babies" },
+      { word: "flamingo", clue: "This animal usually stands on one leg" },
+      { word: "crocodile", clue: "This animal is very snappy" },
+      { word: "jellyfish", clue: "This animal might sting you in the sea" },
+      { word: "squirrel", clue: "This animal loves to climb trees" },
+      { word: "koala bear", clue: "This animal is found in Australia" },
+      { word: "panda", clue: "This animal loves to eat bamboo" },
+      { word: "chicken", clue: "This animal lays eggs" },
+      { word: "cheetah", clue: "This animal is the fastest land animal" },
+      { word: "beaver", clue: "This animal builds dams" },
+      {
+        word: "penguin",
+        clue: "This animal is the only bird that can swim but can't fly",
+      },
+      {
+        word: "parrot",
+        clue: "This animal is a bird that can talk",
+      },
+      {
+        word: "peacock",
+        clue: "This animal is a bird that has magnificent multi-coloured feathers",
+      },
     ],
   },
   {
@@ -26,6 +35,84 @@ const wordCategories = [
     options: [
       { word: "pumpkin", clue: "You can carve a face into this vegetable" },
       { word: "black cat", clue: "A witch might have this pet" },
+      { word: "broomstick", clue: "A witch flies on one of these" },
+      { word: "cauldron", clue: "You would mix potions in one of these" },
+      { word: "wizard", clue: "A man who has magic powers" },
+      { word: "vampire", clue: "These mythical people do not like garlic" },
+      { word: "bat", clue: "Vampires can turn into this flying animal" },
+      { word: "cobweb", clue: "A spider spins one of these" },
+      {
+        word: "werewolf",
+        clue: "A mythical animal someone transforms into during a full moon",
+      },
+      { word: "witch", clue: "A woman who casts magic spells" },
+      {
+        word: "costume",
+        clue: "You would wear one of these to a Halloween party",
+      },
+      { word: "dracula", clue: "He is a famous vampire" },
+      {
+        word: "trick or treat",
+        clue: "A phrase you say when somebody opens the door on Halloween",
+      },
+    ],
+  },
+  {
+    name: "harryPotter",
+    options: [
+      { word: "harry potter", clue: "He is the boy who lived" },
+      { word: "ron weasley", clue: "He has a rat called scabbers" },
+      { word: "hermione granger", clue: "Her parents are both muggles" },
+      { word: "draco malfoy", clue: "His best friends are Crabbe and Goyle" },
+      { word: "crookshanks", clue: "He is Hermione's pet cat" },
+      {
+        word: "neville longbottom",
+        clue: "He is a talented herbology student",
+      },
+      {
+        word: "ginny weasley",
+        clue: "She is the youngest of the Weasley family",
+      },
+      {
+        word: "professor dumbledore",
+        clue: "He is the wise headmaster of Hogwarts",
+      },
+      { word: "voldemort", clue: "He is known as the Dark Lord" },
+      { word: "scabbers", clue: "He was Ron's pet rat" },
+      { word: "dobby", clue: "He is a kind house elf" },
+      {
+        word: "luna lovegood",
+        clue: "She is a Ravenclaw witch who believes in Nargles",
+      },
+      { word: "hedwig", clue: "He is Harry's pet owl" },
+      { word: "sirius black", clue: "He is Harry's godfather" },
+      { word: "rubeus hagrid", clue: "He is the gamekeeper at Hogwards" },
+      {
+        word: "madame hooch",
+        clue: "She taught Hogwarts students how to play Quidditch",
+      },
+      { word: "severus snape", clue: "He was head of Slytherin house" },
+    ],
+  },
+  {
+    name: "superheroes",
+    options: [
+      { word: "spiderman", clue: "This superhero can climb up buildings" },
+      { word: "batman", clue: "This superhero has a butler called Alfred" },
+      { word: "superman", clue: "This superhero's real name is Clark Kent" },
+      { word: "the hulk", clue: "This superhero turns green when he is angry" },
+      { word: "black widow", clue: "She is a spy and one of the Avengers" },
+      { word: "thor", clue: "His brother is Loki" },
+      { word: "iron man", clue: "This superhero owns Stark Industries" },
+      { word: "black panther", clue: "This superhero is the King of Wakanda" },
+      { word: "catwoman", clue: "She is a Gotham City burglar" },
+      {
+        word: "the joker",
+        clue: "He is known as the Clown Prince of Crime",
+      },
+      { word: "lex luthor", clue: "He is Superman's arch-nemesis" },
+      { word: "lex luthor", clue: "He is Superman's arch-nemesis" },
+      { word: "loki", clue: "He is the brother of Thor" },
     ],
   },
 ];
