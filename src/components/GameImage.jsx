945da367bc1004de508b6118040224922a6b6bc7
Rachel <rachelanna92@outlook.com>
2021-10-09 16:03:34 +0100

    use relative image paths for game images

--- src/components/GameImage.jsx
+++ src/components/GameImage.jsx
@@ -2,23 +2,23 @@ const GameImage = ({currLives, winStatus, restartGame}) => {
 
     if (winStatus) {
         return (
-            <img className="GameImage" src="/game-images/pig-win.png" alt="pig flying with wings" onClick={() => {
+            <img className="GameImage" src="./game-images/pig-win.png" alt="pig flying with wings" onClick={() => {
                 restartGame();
             }}/>
         )
     } else if (currLives === 0) {
         return (
-            <img className="GameImage" src="/game-images/pig-0.png" alt="pig on the ground sat in a mud puddle" onClick={() => {
+            <img className="GameImage" src="./game-images/pig-0.png" alt="pig on the ground sat in a mud puddle" onClick={() => {
                 restartGame();
             }}/>
         )
     }else if (currLives === 1) {
         return (
-            <img className="GameImage" src="/game-images/pig-1.png" alt="pig with 1 balloon"/>
+            <img className="GameImage" src="./game-images/pig-1.png" alt="pig with 1 balloon"/>
         )
     } else {
         return (
-            <img className="GameImage" src={`/game-images/pig-${currLives}.png`} alt={`pig with ${currLives} balloons`}/>
+            <img className="GameImage" src={`./game-images/pig-${currLives}.png`} alt={`pig with ${currLives} balloons`}/>
         )
     }
 
